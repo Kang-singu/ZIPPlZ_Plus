dc59ef2a353df3774c43070044af280bf8b68271
unknown <qwerasd0411@naver.com>
2025-08-10 19:20:57 +0900

    Fix redis config 파일에 어노테이션 추가 수정

--- backend/src/main/java/com/example/zipplz_be/Redis/config/RedisConfig.java
+++ backend/src/main/java/com/example/zipplz_be/Redis/config/RedisConfig.java
@@ -1,9 +1,11 @@
 package com.example.zipplz_be.Redis.config;
 
 import com.example.zipplz_be.Redis.service.RedisSubscriber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
